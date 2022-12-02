7801ef728e99c3fd522f759b158d9bfbcb795caa
RafaelJCamara <rafaelcamarac@gmail.com>
2022-12-02 21:21:07 +0000

    Add basic identity server.

---
+++ src/Play.Identity.Service/Play.Identity.Service/Dtos/Dtos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Play.Identity.Service.Dtos
+{
+    public class Dtos
+    {
+        public record UserDto(Guid id, string username, string Email, decimal Gil, DateTimeOffset CreatedDate);
+        public record UpdateUserDto([Required] [EmailAddress] string Email, [Range(0, 1000000)] decimal Gil);
+    }
+}
---
+++ src/Play.Identity.Service/Play.Identity.Service/Entities/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDbGenericRepository.Attributes;
+using System;
+
+namespace Play.Identity.Service.Entities
+{
+    /*
+        We use this attribute, because otherwise our collectionName would be applicationUser
+        To better up the name of the collection, we use this attribute
+     */
+    [CollectionName("Users")]
+    public class ApplicationUser : MongoIdentityUser<Guid>
+    {
+        public decimal Gil { get; set; }
+    }
+}
